5189d3d25c8a46933c64490699e64428dc5a31c5
Timaalattar <fatima.alattar@gmail.com>
2022-10-27 09:30:22 +0300

    finished one part of the classroom function

--- controllers/classroom.js
+++ controllers/classroom.js
@@ -2,10 +2,26 @@ const express = require('express')
 const db = require('../models')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.render('classroom/index.ejs')
-})
+router.get('/', async (req, res)=>{
+    try  {
+      const user = await db.user.findAll() // res.locals.user.id // res.locals.user.id
+    
+      const listQuestion = await db.askquestions.findAll()
+        // user.dataValues.fullName
+        // db,getUser ({fullName: req.body.fullName}))
+        // await user.addAskQuestions(viewQuestion)
+        // await user.getAskQuestions()
+    
+          console.log('question viewed')
+          res.render('classroom/index.ejs', { listQuestions: listQuestion} )
+    
+      
+      } catch (err) {
+         console.log(err) 
+         res.json(err)
+      }
 
+})
 
 
 
